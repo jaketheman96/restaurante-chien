e6a5546dfcc3bfed789f2d8c26febcea308c6ba8
Jake Chien <jake_chien@outlook.com>
2023-01-17 13:16:11 -0300

    chore: add's error handler as middleware

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,11 @@
 const express = require('express');
+const { errorHandler } = require('./src/middlewares/errorHandler');
 
 const app = express();
 
 app.use(express.json());
 app.get('/users', () => console.log('alo'))
 
-app.use((error, _req, res, _next) => {
-  if (error.status) return res.status(error.status).json({ message: error.message });
-  return res.status(500).json({ message: error.message });
-});
+app.use(errorHandler);
 
 module.exports = app;
