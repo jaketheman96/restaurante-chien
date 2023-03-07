df08651e88aca1180c2f31f2bed9574a81073098
Jake Chien <jake_chien@outlook.com>
2023-03-06 21:57:11 -0300

    refactor: remove token validator on foods route

--- backend/src/routes/foods.ts
+++ backend/src/routes/foods.ts
@@ -1,31 +1,25 @@
 import express from 'express';
 import FoodController from '../api/controllers/food.controller';
-import TokenValidator from '../middlewares/tokenValidator';
 
 const foodRouter = express.Router();
 
 foodRouter.get('/',
-  (req, res, next) => new TokenValidator(req, res, next).validator(),
   (req, res, next) => new FoodController(req, res, next).getAllFoods()
 )
 
 foodRouter.get('/:id',
-  (req, res, next) => new TokenValidator(req, res, next).validator(),
   (req, res, next) => new FoodController(req, res, next).getFoodById()
 )
 
 foodRouter.post('/',
-  (req, res, next) => new TokenValidator(req, res, next).validator(),
   (req, res, next) => new FoodController(req, res, next).createFood()
 )
 
 foodRouter.put('/:id',
-  (req, res, next) => new TokenValidator(req, res, next).validator(),
   (req, res, next) => new FoodController(req, res, next).updateFood()
 )
 
 foodRouter.delete('/:id',
-  (req, res, next) => new TokenValidator(req, res, next).validator(),
   (req, res, next) => new FoodController(req, res, next).deleteFood()
 )
 
