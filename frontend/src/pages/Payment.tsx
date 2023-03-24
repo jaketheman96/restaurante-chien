e0baef8479ac7dc8a48b15b4d81c1752f28ec670
Jake Chien <jake_chien@outlook.com>
2023-03-24 19:38:11 -0300

    feat: payment done and user redirects to portal

--- frontend/src/pages/Payment.tsx
+++ frontend/src/pages/Payment.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import PortalNavbar from '../components/PortalNavbar';
@@ -11,17 +11,33 @@ function Payment() {
   const navigate = useNavigate();
   const { totalPrice } = useCart();
   const [paymentMethod, setPaymentMethod] = useState('');
-  const FIVE_SECONDS = 5000;
-  
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const [isPaymentDone, setIsPaymentDone] = useState(false);
+  const TWO_SECONDS = 2000;
+  const SIX_SECONDS = 6000;
+
   const handleClick = () => {
     if (paymentMethod !== '') {
       dispatch(setIsLoading(true));
       dispatch(selectPaymentMethod({ paymentMethod }));
-      setTimeout(() => dispatch(setIsLoading(false)), FIVE_SECONDS);
-      return navigate('/payment/success');
+      setTimeout(() => {
+        dispatch(setIsLoading(false));
+        setShowSuccessMessage(true);
+        setIsPaymentDone(true);
+      }, TWO_SECONDS);
+      return;
     }
     return;
   };
+
+  useEffect(() => {
+    const redirectUser = () => {
+      if (isPaymentDone) {
+        setTimeout(() => navigate('/portal'), SIX_SECONDS);
+      }
+    };
+    redirectUser();
+  }, [isPaymentDone, navigate]);
 
   const handleSelectChange = (event: ChangeEvent<HTMLSelectElement>) => {
     const target = event.target as HTMLSelectElement;
@@ -40,16 +56,23 @@ function Payment() {
           onChange={handleSelectChange}
         >
           <option value=''>-----</option>
-          <option value='credit'>Crédito</option>
-          <option value='debit'>Débito</option>
-          <option value='pix'>Pix</option>
-          <option value='congrats'>Meus parabéns!</option>
+          <option value='Crédito'>Crédito</option>
+          <option value='Débito'>Débito</option>
+          <option value='Pix'>Pix</option>
+          <option value='um abraço'>Um abraço</option>
         </select>
       </label>
-      <button type='button'>Voltar</button>
+      <button type='button' onClick={() => navigate('/checkout')}>
+        Voltar
+      </button>
       <button type='button' onClick={handleClick}>
         Continuar
       </button>
+      {showSuccessMessage && (
+        <p>
+          {`Pagamento concluído com sucesso, você será redirecionado em instantes.`}
+        </p>
+      )}
     </>
   );
 }
